9093947a456cb24207ae27e10e8629816ba41bd8
pronobiswas <biswaspronob@outlook.com>
2025-07-19 18:19:39 +0600

    update

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import HomePage from "./pages/HomePage";
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  Outlet,
+} from "react-router-dom";
+import RootLayout from "./rootLayout/RootLayout";
+
+function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route element={<RootLayout />}>
+          <Route path="/" element={<HomePage />} />
+        </Route>
+      </>
+    )
+  );
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="w-full h-screen bg-black">
+      <h1>this is home page</h1>
+    </div>
+  );
+};
+
+export default HomePage;
